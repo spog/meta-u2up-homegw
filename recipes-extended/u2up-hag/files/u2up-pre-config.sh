f24868ff3b2d066bb3abff55a487c96c639a7cdd
Samo Pogačnik <samo_pogacnik@t-2.net>
2019-04-30 17:17:59 +0200

    First successfull "hostname pre-config" on first target boot.

--- recipes-extended/u2up-hag/files/u2up-pre-config.sh
+++ recipes-extended/u2up-hag/files/u2up-pre-config.sh
@@ -7,13 +7,15 @@
 U2UP_INSTALL_BASH_LIB="/lib/u2up/u2up-install-bash-lib"
 if [ ! -f "${U2UP_INSTALL_BASH_LIB}" ]; then
 	echo "Program terminated (missing: ${U2UP_INSTALL_BASH_LIB})!"
-	/bin/systemctl isolate multi-user.target
+#	/bin/systemctl isolate multi-user.target
 	exit 1
 fi
 source ${U2UP_INSTALL_BASH_LIB}
 
 echo "Hello" > /dev/console
-sleep 10
+#sleep 10
+set -x
+evaluate_u2up_configurations &> /dev/console
 echo "Bye" > /dev/console
 
 #/bin/systemctl isolate multi-user.target &> /dev/console
